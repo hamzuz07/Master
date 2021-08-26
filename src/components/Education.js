8a7b1b9786174b4536d3bd9024fede2b6585f53a
unknown <hamzatouati2012@gmail.com>
2021-08-26 01:31:05 +0100

    my project

---
+++ src/components/Education.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import Home from "./Home";
+
+
+function Education (){
+    
+return(
+    
+<body className="bg1">
+ <div className="title-education">Test</div>
+
+
+</body>
+)
+}
+export default Education;
---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Home.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import { Image } from 'react';
+
+
+
+
+function Home (){
+    
+return(
+    
+<body className="bg1">
+
+
+
+
+ <div  className="title-eduction">
+Education 
+</div>
+<div  className="title-name">
+Hamza Touati 
+<h3>Computer enginner </h3>
+   <h3> web developer
+</h3>
+</div>
+<div  className="title-exper">
+Experience 
+</div> 
+
+
+</body>
+)
+}
+export default Home;
